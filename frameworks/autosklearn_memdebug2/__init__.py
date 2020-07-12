dada60ab6432bc403433ed5d4d489d1511b1b5e1
chico <francisco.rivera.valverde@gmail.com>
2020-07-12 15:45:55 +0200

    push

--- frameworks/autosklearn_memdebug2/__init__.py
+++ frameworks/autosklearn_memdebug2/__init__.py
@@ -11,18 +11,35 @@ def setup(*args, **kwargs):
 def run(dataset: Dataset, config: TaskConfig):
     from frameworks.shared.caller import run_in_venv
 
-    data = dict(
-        train=dict(
-            X_enc=dataset.train.X_enc,
-            y_enc=dataset.train.y_enc
-        ),
-        test=dict(
-            X_enc=dataset.test.X_enc,
-            y_enc=dataset.test.y_enc
-        ),
+    try:
+        print(f"dataset.train.X_enc={dataset.train.X_enc}")
+        print(f"dataset.train.y_enc={dataset.train.y_enc}")
+        print(f"dataset.test.X_enc={dataset.test.X_enc}")
+        print(f"dataset.test.y_enc={dataset.test.y_enc}")
+        print(f"dataset.predictors={dataset.predictors}")
         predictors_type=['Categorical' if p.is_categorical() else 'Numerical' for p in dataset.predictors]
-    )
+        print(f"predictors_type={predictors_type}")
+        for p in dataset.predictors:
+            print(f"p={p} type={type(p)}")
+            print(f"p.is_categorical()={p.is_categorical()}")
 
+        data = dict(
+            train=dict(
+                X_enc=dataset.train.X_enc,
+                y_enc=dataset.train.y_enc
+            ),
+            test=dict(
+                X_enc=dataset.test.X_enc,
+                y_enc=dataset.test.y_enc
+            ),
+            predictors_type=['Categorical' if p.is_categorical() else 'Numerical' for p in dataset.predictors]
+        )
+        print(f)
+    except Exception as e:
+        print(f"Exception={e}")
+        raise e
+
+    print(f"Input data is data={data}")
     return run_in_venv(__file__, "exec.py",
                        input_data=data, dataset=dataset, config=config)
 
